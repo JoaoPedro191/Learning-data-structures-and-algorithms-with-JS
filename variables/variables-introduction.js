70bd0dd6ad4084e646d181333394bff089f71767
JoaoPedro191 <sasc.joao@gmail.com>
2024-10-29 18:19:11 -0300

    Variables-introduction

---
+++ variables/variables-introduction.js
@@ -0,0 +1,29 @@
+/* 
+1- As variáveis armazenam dados que podem ser definidos, atualizados, e recuperados;
+2- Existem alguns tipos de dados importantes que uma variável consegue armazenar: number -> (número), string -> (texto), null -> (nulo), undefined -> (indefinido),
+   boolean -> (boleano = valores -> true ou false), object -> (objeto), functions -> (funções), arrays -> (lista)
+*/
+
+/*-variables examples*/
+var num = 1; //output -> {1} = number;
+num = 3; // output of num -> {3} = number;
+var price = 1.5; // -> output {1.5} = number;
+var myName = "John"; // -> output {John} = string;
+var trueValue = true; // -> output {true} = boolean;
+var nullVar = null; // -> output {null} null;
+var und; // -> output {undefined} undefined;
+
+/* print of values of varibles in browser*/
+console.log("num : " + num); // -> concatenated values;
+console.log("price " + price); // -> concatenated values;
+console.log("myName " + myName); // -> concatenated values;
+console.log("trueValue " + trueValue); // -> concatenated values;
+console.log("nullVar " + nullVar); // -> concatenated values;
+console.log("und " + und); // -> concatenated values;
+
+console.log("num : ", num); // -> values ​​without concatenation;
+console.log("price ", price); // -> values ​without concatenation;
+console.log("myName ", myName); // -> values ​without concatenation;
+console.log("trueValue ", trueValue); // -> values ​without concatenation;
+console.log("nullVar ", nullVar); // -> values ​without concatenation;
+console.log("und ", und); // -> values ​without concatenation;
